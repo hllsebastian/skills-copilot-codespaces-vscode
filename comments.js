95c27109375cb8406d8267ab0a535bc52015c66f
hllsebastian <84684391+hllsebastian@users.noreply.github.com>
2023-11-26 20:00:53 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,74 +1,49 @@
-// create new web server
+// Create web server
+ // ----------------
+
 const express = require('express');
-const app = express();
-// create new database connection
-const db = require('./database.js');
-// create new web server
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
+const axios = require('axios');
+
+const app = express();
 app.use(bodyParser.json());
-// create new web server
-const HTTP_PORT = 8000;
-app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT));
+
+// Store comments in memory
+const commentsByPostId = {};
+
+// Handle requests to /posts/:id/comments
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
 });
-// create new web server
-// Root path
-app.get("/", (req, res, next) => {
-    res.json({"message": "Ok"});
+
+// Handle requests to /posts/:id/comments
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = Math.random().toString(36).substring(2);
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+  commentsByPostId[req.params.id] = comments;
+
+  // Emit an event to the Event Bus
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
+
+  res.status(201).send(comments);
 });
-// create new web server
-// GET
-app.get("/api/comments", (req, res, next) => {
-    const sql = "select * from comments";
-    const params = [];
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-            res.status(400).json({"error": err.message});
-            return;
-        }
-        res.json({
-            "message": "success",
-            "data": rows
-        });
-    });
+
+// Handle requests to /events
+app.post('/events', (req, res) => {
+  console.log('Received event', req.body.type);
+  res.send({});
 });
-// create new web server
-// GET
-app.get("/api/comments/:id", (req, res, next) => {
-    const sql = "select * from comments where rowid = ?";
-    const params = [req.params.id];
-    db.get(sql, params, (err, row) => {
-        if (err) {
-            res.status(400).json({"error": err.message});
-            return;
-        }
-        res.json({
-            "message": "success",
-            "data": row
-        });
-    });
+
+// Start server on port 4001
+app.listen(4001, () => {
+  console.log('Listening on 4001');
 });
-// create new web server
-// POST
-app.post("/api/comments/", (req, res, next) => {
-    const errors = [];
-    if (!req.body.email) {
-        errors.push("No email specified");
-    }
-    if (!req.body.comment) {
-        errors.push("No comment specified");
-    }
-    if (errors.length) {
-        res.status(400).json({"error": errors.join(",")});
-        return;
-    }
-    const data = {
-        email: req.body.email,
-        comment: req.body.comment
-    };
-    const sql = 'INSERT INTO comments (email, comment) VALUES (?,?)';
-    const params = [data.email, data.comment];
-    db.run(sql, params, function (err, result) {
-        if (err) {
-            res
